fb80e7e602b70a4a21fa7ddee910b9cf28312b21
s_balema <sviatoslavbalema@gmail.com>
2025-08-02 17:54:46 +0200

    Calculator created.

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
+import re
 
-def main():
-    print("Hi.")
+def perform_math():
+    run = True
+    equation = None
+    previous = ''
+
+    print("Welcome to the calculator.")
+    print("Type 'quit' to exit.")
+
+    while run:
+        if previous == '':
+            equation = input("Starting with: ")
+        else:
+            equation = input(f"{str(previous)}")
+
+        if equation == "quit":
+            run = False
+        else:
+            equation = re.sub('[a-zA-Z,:()" "]', '', equation)
+            if equation == '':
+                print("You typed: ", equation)
+            else:
+                previous = eval(str(previous) + equation)
+                print("You typed:", equation)
+
+    print("Exiting...")
+    return
 
 if __name__ == '__main__':
-    main()
+    perform_math()
