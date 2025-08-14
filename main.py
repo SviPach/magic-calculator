b4bb930baf5c3d4da9d8560b03afce54865e56c2
s_balema <sviatoslavbalema@gmail.com>
2025-08-14 19:16:48 +0200

    docs: change ':' -> '->' in the comments style: improve the code's style rework: rework string cleaning feat: remove '*' and '/' if those are the first symbols in new equation feat: if there is "*/" ot "/*" in equation - delete second character

--- main.py
+++ main.py
@@ -17,32 +17,46 @@ def perform_math():
     print("Type 'quit' to exit.")
 
     while run:
-        # If there has been a previous calculation:
+        # If there has been a previous calculation ->
         if previous == '':
             equation = input("Starting with: ")
         else:
             equation = input(f"{str(previous)}")
 
-        # If user quits:
+        # If user quits ->
         if equation == "quit":
             run = False
-        # If user clears the previous calculation:
+
+        # If user clears the previous calculation ->
         elif equation == "clear":
             previous = ''
             print("You cleared the previous calculation.")
             continue
         else:
-            # Cleaning the input string:
-            if previous == '':
-                equation =  re.sub('[a-zA-Z,:()" "/*]', '', equation)
-            else:
-                equation = re.sub('[a-zA-Z,:()" "]', '', equation)
+            # Cleaning the input string ->
+            equation =  re.sub('[a-zA-Z,:()" "]', '', equation)
 
-            # Deleting "." if it's a first symbol:
+            if previous == '':
+                # Deleting '*' and '/' if it's the first symbol ->
+                while equation[0] == '*' or equation[0] == '/':
+                    equation = equation[1:]
+
+            # Check for "*/" and "/*" and if found, delete the second character->
+            equation = list(equation)
+            for i in range(len(equation) - 1):
+                if (
+                        equation[i]+equation[i + 1] == '/*'
+                        or
+                        equation[i]+equation[i + 1] == '*/'
+                ):
+                    equation[i+1] = ''
+            equation = ''.join(equation)
+
+            # Deleting "." if it's a first symbol ->
             if len(equation)>0 and equation[0] == '.':
                 equation = equation[1:]
 
-            # Printing the result:
+            # Printing the result ->
             if equation == '':
                 print("You typed: ", equation)
             else:
